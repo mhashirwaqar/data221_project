5fd3eb2c0613e78691a3a3e291d4ec9c55eda31e
seancheng360@gmail.com <sean.cheng@ucalgary.ca>
2026-03-24 14:19:30 -0600

    train knn model (should work once main is implemented)

--- src/knn_model.py
+++ src/knn_model.py
@@ -1,7 +1,18 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+import numpy as np
+def train_knn(X_train_scaled, y_train, k_values = None, cross_validation = 5):
+    best_f1 = 0
+    best_k = k_values[0]  # default to first value
 
-def train_knn():
+    for k in k_values:
+        knn = KNeighborsClassifier(n_neighbors=k)
+        scores = cross_val_score(knn, X_train_scaled, y_train, cv=cross_validation, scoring='f1')
+        mean_f1 = np.mean(scores)
+        if mean_f1 > best_f1:
+            best_f1 = mean_f1
+            best_k = k
 
-    # write your code here
-    # test commit
-
-    return 0
+    best_model = KNeighborsClassifier(n_neighbors=best_k)
+    best_model.fit(X_train_scaled, y_train)
+    return best_model, best_k
