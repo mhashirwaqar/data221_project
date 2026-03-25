7f3efa034d2cc3f9a23b091e7f28a2e644833bca
MuditJ-007 <mudit.jindal2007@gmail.com>
2026-03-24 22:52:56 -0600

    Added comments in logistic_regression.py

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,8 +1,24 @@
-# TODO: Implement Logistic Regression model here
+# logistic_regression.py
 
+# Import Logistic Regression model from sklearn
 from sklearn.linear_model import LogisticRegression
 
 def train_logistic(X_train_scaled, y_train):
+    """
+    Train a Logistic Regression model.
+
+    Parameters:
+    X_train_scaled : Scaled training feature data
+    y_train : Training labels
+
+    Returns:
+    model : Trained Logistic Regression model
+    """
+
+    # Use class_weight='balanced' because dataset is highly imbalanced (fraud detection)
     model = LogisticRegression(class_weight='balanced')
+
+    # Train the model
     model.fit(X_train_scaled, y_train)
+
     return model
